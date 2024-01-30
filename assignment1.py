684b196e56b5213d4529538679cf060b0eceec1f
Sarah Chao <chaosa@oregonstate.edu>
2024-01-29 20:33:32 -0800

    fixed testing

--- assignment1.py
+++ assignment1.py
@@ -196,11 +196,6 @@ def sorted_squares(arr: StaticArray) -> StaticArray:
     """
     TODO: Write this implementation
     """
-    
-    # squared_ascend = StaticArray(arr.length())
-    # for i in range(arr.length()):
-    #     squared_ascend[i] = arr[i]**2
-    # return squared_ascend
 
 
     positives = StaticArray(arr.length())
@@ -248,166 +243,166 @@ def sorted_squares(arr: StaticArray) -> StaticArray:
 if __name__ == "__main__":
 
 
-    # print('\n# min_max example 1')
-    # arr = StaticArray(5)
-    # for i, value in enumerate([7, 8, 6, -5, 4]):
-    #     arr[i] = value
-    # print(arr)
-    # result = min_max(arr)
-    # if result:
-    #     print(f"Min: {result[0]: 3}, Max: {result[1]}")
-    # else:
-    #     print("min_max() not yet implemented")
+    print('\n# min_max example 1')
+    arr = StaticArray(5)
+    for i, value in enumerate([7, 8, 6, -5, 4]):
+        arr[i] = value
+    print(arr)
+    result = min_max(arr)
+    if result:
+        print(f"Min: {result[0]: 3}, Max: {result[1]}")
+    else:
+        print("min_max() not yet implemented")
 
-    # print('\n# min_max example 2')
-    # arr = StaticArray(1)
-    # arr[0] = 100
-    # print(arr)
-    # result = min_max(arr)
-    # if result:
-    #     print(f"Min: {result[0]}, Max: {result[1]}")
-    # else:
-    #     print("min_max() not yet implemented")
+    print('\n# min_max example 2')
+    arr = StaticArray(1)
+    arr[0] = 100
+    print(arr)
+    result = min_max(arr)
+    if result:
+        print(f"Min: {result[0]}, Max: {result[1]}")
+    else:
+        print("min_max() not yet implemented")
 
-    # print('\n# min_max example 3')
-    # test_cases = (
-    #     [3, 3, 3],
-    #     [-10, -30, -5, 0, -10],
-    #     [25, 50, 0, 10],
-    # )
-    # for case in test_cases:
-    #     arr = StaticArray(len(case))
-    #     for i, value in enumerate(case):
-    #         arr[i] = value
-    #     print(arr)
-    #     result = min_max(arr)
-    #     if result:
-    #         print(f"Min: {result[0]: 3}, Max: {result[1]}")
-    #     else:
-    #         print("min_max() not yet implemented")
+    print('\n# min_max example 3')
+    test_cases = (
+        [3, 3, 3],
+        [-10, -30, -5, 0, -10],
+        [25, 50, 0, 10],
+    )
+    for case in test_cases:
+        arr = StaticArray(len(case))
+        for i, value in enumerate(case):
+            arr[i] = value
+        print(arr)
+        result = min_max(arr)
+        if result:
+            print(f"Min: {result[0]: 3}, Max: {result[1]}")
+        else:
+            print("min_max() not yet implemented")
 
-    # print('\n# fizz_buzz example 1')
-    # source = [_ for _ in range(-5, 20, 4)]
-    # arr = StaticArray(len(source))
-    # for i, value in enumerate(source):
-    #     arr[i] = value
-    # print(fizz_buzz(arr))
-    # print(arr)
+    print('\n# fizz_buzz example 1')
+    source = [_ for _ in range(-5, 20, 4)]
+    arr = StaticArray(len(source))
+    for i, value in enumerate(source):
+        arr[i] = value
+    print(fizz_buzz(arr))
+    print(arr)
 
-    # print('\n# reverse example 1')
-    # source = [_ for _ in range(-20, 20, 7)]
-    # arr = StaticArray(len(source))
-    # for i, value in enumerate(source):
-    #     arr.set(i, value)
-    # print(arr)
-    # reverse(arr)
-    # print(arr)
-    # reverse(arr)
-    # print(arr)
+    print('\n# reverse example 1')
+    source = [_ for _ in range(-20, 20, 7)]
+    arr = StaticArray(len(source))
+    for i, value in enumerate(source):
+        arr.set(i, value)
+    print(arr)
+    reverse(arr)
+    print(arr)
+    reverse(arr)
+    print(arr)
 
-    # print('\n# rotate example 1')
-    # source = [_ for _ in range(-20, 20, 7)]
-    # arr = StaticArray(len(source))
-    # for i, value in enumerate(source):
-    #     arr.set(i, value)
-    # print(arr)
-    # for steps in [1, 2, 0, -1, -2, 28, -100, 2 ** 28, -2 ** 31]:
-    #     space = " " if steps >= 0 else ""
-    #     print(f"{rotate(arr, steps)} {space}{steps}")
-    # print(arr)
+    print('\n# rotate example 1')
+    source = [_ for _ in range(-20, 20, 7)]
+    arr = StaticArray(len(source))
+    for i, value in enumerate(source):
+        arr.set(i, value)
+    print(arr)
+    for steps in [1, 2, 0, -1, -2, 28, -100, 2 ** 28, -2 ** 31]:
+        space = " " if steps >= 0 else ""
+        print(f"{rotate(arr, steps)} {space}{steps}")
+    print(arr)
 
-    # print('\n# rotate example 2')
-    # array_size = 1_000_000
-    # source = [random.randint(-10 ** 9, 10 ** 9) for _ in range(array_size)]
-    # arr = StaticArray(len(source))
-    # for i, value in enumerate(source):
-    #     arr[i] = value
-    # print(f'Started rotating large array of {array_size} elements')
-    # rotate(arr, 3 ** 14)
-    # rotate(arr, -3 ** 15)
-    # print(f'Finished rotating large array of {array_size} elements')
+    print('\n# rotate example 2')
+    array_size = 1_000_000
+    source = [random.randint(-10 ** 9, 10 ** 9) for _ in range(array_size)]
+    arr = StaticArray(len(source))
+    for i, value in enumerate(source):
+        arr[i] = value
+    print(f'Started rotating large array of {array_size} elements')
+    rotate(arr, 3 ** 14)
+    rotate(arr, -3 ** 15)
+    print(f'Finished rotating large array of {array_size} elements')
 
-    # print('\n# sa_range example 1')
-    # cases = [
-    #     (1, 3), (-1, 2), (0, 0), (0, -3),
-    #     (-95, -89), (-89, -95)]
-    # for start, end in cases:
-    #     print(f"Start: {start: 4}, End: {end: 4}, {sa_range(start, end)}")
+    print('\n# sa_range example 1')
+    cases = [
+        (1, 3), (-1, 2), (0, 0), (0, -3),
+        (-95, -89), (-89, -95)]
+    for start, end in cases:
+        print(f"Start: {start: 4}, End: {end: 4}, {sa_range(start, end)}")
 
-    # print('\n# is_sorted example 1')
-    # test_cases = (
-    #     [-100, -8, 0, 2, 3, 10, 20, 100],
-    #     ['A', 'B', 'Z', 'a', 'z'],
-    #     ['Z', 'T', 'K', 'A', '5'],
-    #     [1, 3, -10, 20, -30, 0],
-    #     [-10, 0, 0, 10, 20, 30],
-    #     [100, 90, 0, -90, -200],
-    #     ['apple']
-    # )
-    # for case in test_cases:
-    #     arr = StaticArray(len(case))
-    #     for i, value in enumerate(case):
-    #         arr[i] = value
-    #     result = is_sorted(arr)
-    #     space = "  " if result and result >= 0 else " "
-    #     print(f"Result:{space}{result}, {arr}")
+    print('\n# is_sorted example 1')
+    test_cases = (
+        [-100, -8, 0, 2, 3, 10, 20, 100],
+        ['A', 'B', 'Z', 'a', 'z'],
+        ['Z', 'T', 'K', 'A', '5'],
+        [1, 3, -10, 20, -30, 0],
+        [-10, 0, 0, 10, 20, 30],
+        [100, 90, 0, -90, -200],
+        ['apple']
+    )
+    for case in test_cases:
+        arr = StaticArray(len(case))
+        for i, value in enumerate(case):
+            arr[i] = value
+        result = is_sorted(arr)
+        space = "  " if result and result >= 0 else " "
+        print(f"Result:{space}{result}, {arr}")
 
-    # print('\n# find_mode example 1')
-    # test_cases = (
-    #     [1, 20, 30, 40, 500, 500, 500],
-    #     [2, 2, 2, 2, 1, 1, 1, 1],
-    #     ["zebra", "sloth", "otter", "otter", "moose", "koala"],
-    #     ["Albania", "Belgium", "Chile", "Denmark", "Egypt", "Fiji"]
-    # )
-    # for case in test_cases:
-    #     arr = StaticArray(len(case))
-    #     for i, value in enumerate(case):
-    #         arr[i] = value
+    print('\n# find_mode example 1')
+    test_cases = (
+        [1, 20, 30, 40, 500, 500, 500],
+        [2, 2, 2, 2, 1, 1, 1, 1],
+        ["zebra", "sloth", "otter", "otter", "moose", "koala"],
+        ["Albania", "Belgium", "Chile", "Denmark", "Egypt", "Fiji"]
+    )
+    for case in test_cases:
+        arr = StaticArray(len(case))
+        for i, value in enumerate(case):
+            arr[i] = value
 
-    #     result = find_mode(arr)
-    #     if result:
-    #         print(f"{arr}\nMode: {result[0]}, Frequency: {result[1]}\n")
-    #     else:
-    #         print("find_mode() not yet implemented\n")
+        result = find_mode(arr)
+        if result:
+            print(f"{arr}\nMode: {result[0]}, Frequency: {result[1]}\n")
+        else:
+            print("find_mode() not yet implemented\n")
 
-    # print('# remove_duplicates example 1')
-    # test_cases = (
-    #     [1], [1, 2], [1, 1, 2], [1, 20, 30, 40, 500, 500, 500],
-    #     [5, 5, 5, 4, 4, 3, 2, 1, 1], [1, 1, 1, 1, 2, 2, 2, 2]
-    # )
-    # for case in test_cases:
-    #     arr = StaticArray(len(case))
-    #     for i, value in enumerate(case):
-    #         arr[i] = value
-    #     print(arr)
-    #     print(remove_duplicates(arr))
-    # print(arr)
+    print('# remove_duplicates example 1')
+    test_cases = (
+        [1], [1, 2], [1, 1, 2], [1, 20, 30, 40, 500, 500, 500],
+        [5, 5, 5, 4, 4, 3, 2, 1, 1], [1, 1, 1, 1, 2, 2, 2, 2]
+    )
+    for case in test_cases:
+        arr = StaticArray(len(case))
+        for i, value in enumerate(case):
+            arr[i] = value
+        print(arr)
+        print(remove_duplicates(arr))
+    print(arr)
 
-    # print('\n# count_sort example 1')
-    # test_cases = (
-    #     [1, 2, 4, 3, 5], [5, 4, 3, 2, 1], [0, -5, -3, -4, -2, -1, 0],
-    #     [-3, -2, -1, 0, 1, 2, 3], [1, 2, 3, 4, 3, 2, 1, 5, 5, 2, 3, 1],
-    #     [10100, 10721, 10320, 10998], [-100320, -100450, -100999, -100001],
-    # )
-    # for case in test_cases:
-    #     arr = StaticArray(len(case))
-    #     for i, value in enumerate(case):
-    #         arr[i] = value
-    #     print(f"Before: {arr}")
-    #     result = count_sort(arr)
-    #     print(f"After : {result}")
+    print('\n# count_sort example 1')
+    test_cases = (
+        [1, 2, 4, 3, 5], [5, 4, 3, 2, 1], [0, -5, -3, -4, -2, -1, 0],
+        [-3, -2, -1, 0, 1, 2, 3], [1, 2, 3, 4, 3, 2, 1, 5, 5, 2, 3, 1],
+        [10100, 10721, 10320, 10998], [-100320, -100450, -100999, -100001],
+    )
+    for case in test_cases:
+        arr = StaticArray(len(case))
+        for i, value in enumerate(case):
+            arr[i] = value
+        print(f"Before: {arr}")
+        result = count_sort(arr)
+        print(f"After : {result}")
 
-    # print('\n# count_sort example 2')
-    # array_size = 5_000_000
-    # min_val = random.randint(-1_000_000_000, 1_000_000_000 - 998)
-    # max_val = min_val + 998
-    # case = [random.randint(min_val, max_val) for _ in range(array_size)]
-    # arr = StaticArray(len(case))
-    # for i, value in enumerate(case):
-    #     arr[i] = value
-    # print(f'Started sorting large array of {array_size} elements')
-    # result = count_sort(arr)
-    # print(f'Finished sorting large array of {array_size} elements')
+    print('\n# count_sort example 2')
+    array_size = 5_000_000
+    min_val = random.randint(-1_000_000_000, 1_000_000_000 - 998)
+    max_val = min_val + 998
+    case = [random.randint(min_val, max_val) for _ in range(array_size)]
+    arr = StaticArray(len(case))
+    for i, value in enumerate(case):
+        arr[i] = value
+    print(f'Started sorting large array of {array_size} elements')
+    result = count_sort(arr)
+    print(f'Finished sorting large array of {array_size} elements')
 
     print('\n# sorted_squares example 1')
     test_cases = (
